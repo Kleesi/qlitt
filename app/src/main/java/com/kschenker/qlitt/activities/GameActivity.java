50eae24f45c761229e5ca90949c169a9ee885f04
Kevin Schenker <schenker.kevin@gmail.com>
2016-04-11 19:30:10 +0200

    Initial commit

---
+++ app/src/main/java/com/kschenker/qlitt/activities/GameActivity.java
@@ -0,0 +1,17 @@
+package com.kschenker.qlitt.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kschenker.qlitt.R;
+
+public class GameActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__game);
+    }
+}
