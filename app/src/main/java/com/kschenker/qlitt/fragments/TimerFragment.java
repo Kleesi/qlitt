f6c565f09593460ec6b0df22c6b9510c293ffb01
Kevin Schenker <schenker.kevin@gmail.com>
2016-04-23 17:44:13 +0200

    Added a timer fragment which is responsible for displaying the remaining time.

---
+++ app/src/main/java/com/kschenker/qlitt/fragments/TimerFragment.java
@@ -0,0 +1,24 @@
+package com.kschenker.qlitt.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kschenker.qlitt.R;
+
+public class TimerFragment extends Fragment
+{
+    public TimerFragment()
+    {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment__timer, container, false);
+    }
+}
