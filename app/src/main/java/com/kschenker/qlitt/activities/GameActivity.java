faab3ff4d875b1de81771f3525968fe786451969
Kevin Schenker <schenker.kevin@gmail.com>
2016-04-25 17:15:55 +0200

    Added some code to print the available space in DP that is also used in correspondence to the resource qualifiers wxdp and hxdp.

--- app/src/main/java/com/kschenker/qlitt/activities/GameActivity.java
+++ app/src/main/java/com/kschenker/qlitt/activities/GameActivity.java
@@ -1,5 +1,6 @@
 package com.kschenker.qlitt.activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
@@ -12,12 +13,22 @@ import com.kschenker.qlitt.R;
 public class GameActivity extends AppCompatActivity
 {
 
+    private static final String LOG_TAG_DISPLAY = "Display";
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__game);
+        printAvailableSizeForUI();
         addGlobalLayoutListener();
+    }
+
+    private void printAvailableSizeForUI()
+    {
+        String messageFormat = "Available size is (%ddp, %ddp)";
+        Configuration configuration = getResources().getConfiguration();
+        Log.i(LOG_TAG_DISPLAY, String.format(messageFormat, configuration.screenWidthDp, configuration.screenHeightDp));
     }
 
     private void addGlobalLayoutListener()
@@ -50,7 +61,7 @@ public class GameActivity extends AppCompatActivity
             float heightInDip = view.getHeight() / displayMetrics.density;
 
             String sizeMessageFormat = "Size of %s is (%.2fdp, %.2fdp)";
-            Log.i("Display", String.format(sizeMessageFormat, name, widthInDip, heightInDip));
+            Log.i(LOG_TAG_DISPLAY, String.format(sizeMessageFormat, name, widthInDip, heightInDip));
         }
     }
 
